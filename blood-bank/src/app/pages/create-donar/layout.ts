8aa177676fadd94b0fc672015c212079f07c1fb1
Kaleeswaran <kaleeswaran@iinerds.com>
2020-01-25 23:29:18 +0530

    static table added

--- blood-bank/src/app/pages/create-donar/layout.ts
+++ blood-bank/src/app/pages/create-donar/layout.ts
@@ -24,7 +24,61 @@ export class CreateLayout {
                 model: '',
                 options: this.donorModel.bloodGroupList
             },
+            {
+                label: 'Gender',
+                type: 'dropDown',
+                value: '',
+                model: '',
+                options: [{ name: 'male', value: 'Male' }, { name: 'female', value: 'Female' }]
+            },
+
+            {
+                label: 'E-mail',
+                type: 'input',
+                value: '',
+                model: ''
+            }
 
         ]
     }
+
+    addressLayout = {
+        header: "Address & communication",
+        fields: [
+            {
+                label: 'place',
+                type: 'input',
+                value: '',
+                model: ''
+            },
+            {
+                label: 'city',
+                type: 'input',
+                value: 'city',
+                model: ''
+            },
+            {
+                label: 'state',
+                type: 'dropDown',
+                value: '',
+                model: '',
+                options: this.donorModel.stateList
+            },
+            {
+                label: 'pincode',
+                type: 'input',
+                value: '',
+                model: '',
+            },
+            {
+                label: 'Phone Number',
+                type: 'input',
+                value: '',
+                model: ''
+            }
+
+        ]
+    }
+
+
 }
--- blood-bank/src/app/pages/create-donar/model.ts
+++ blood-bank/src/app/pages/create-donar/model.ts
@@ -7,4 +7,8 @@ export class CreateDonor {
     { name: 'aNeg', value: 'A-ve' }, { name: 'bPos', value: 'B+ve' },
     { name: 'BNeg', value: 'B-ve' }, { name: 'oPos', value: 'O+ve' },
     { name: 'oNeg', value: 'O-ve' }, { name: 'abNeg', value: 'AB-ve' }, { name: 'abPos', value: 'AB+ve' }]
+
+    stateList = [{ name: 'andra', value: 'Aandra' }, { name: 'bengal', value: 'Bengal' },
+    { name: 'karnataka', value: 'Karnataka' }, { name: 'kerala', value: 'Kerala' },
+    { name: 'tamilnadu', value: 'Tamilnadu' }]
 }
