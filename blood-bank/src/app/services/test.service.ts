2f4bdd9660c0e812c5355a393133029af5ff8e03
Kaleeswaran <kaleeswaran@iinerds.com>
2020-02-19 21:45:05 +0530

    get call

---
+++ blood-bank/src/app/services/test.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  constructor() { }
+
+  //   private _donorList: BehaviorSubject = new BehaviorSubject([] );
+  //   public readonly donorList: Observable = this._donorList.asObservable();
+
+
+  // that._sitesFilter.next( that.da
+}
